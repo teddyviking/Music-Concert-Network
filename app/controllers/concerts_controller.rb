32ad3269b6bcbcb62508412e180b03d29da4b2b6
Rafael Tejado <rtejado90@gmail.com>
2015-05-01 11:21:17 +0200

    Add form to create new concerts

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -8,10 +8,19 @@ class ConcertsController < ApplicationController
   end
 
   def new
+    @concert = Concert.new
   end
 
   def create
-  	
+  	@concert = Concert.new permited_params
+    binding.pry
+    if @concert.save
+      flash.now[:notice] = "Concert successfuly created"
+      redirect_to concert_path(@concert)
+    else
+      flash.now[:alert] = "There are some errors in your concert"
+      render 'new'
+    end
   end
 
   def edit
@@ -25,5 +34,10 @@ class ConcertsController < ApplicationController
   	
   end
 
+  private
+
+  def permited_params
+    params.require(:concert).permit(:band, :venue, :city, :date, :price, :description)
+  end
   
 end
